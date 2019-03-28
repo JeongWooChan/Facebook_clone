442c8cb2ae7119f0a5f83ee079c85e93437eb54f
정우찬 <vdncks25@naver.com>
2019-03-28 21:59:23 +0900

    Add function : check email overlap in sign up > node.js
    회원가입시 이메일 중복체크기능 추가

--- index.js
+++ index.js
@@ -1,41 +1,54 @@
 console.log('hello');
 
-var express = require('express'); 
-var app = express(); 
-var db = require('./js/db.js'); 
-var bodyParser = require('body-parser'); 
+var express = require('express');
+var app = express();
+var db = require('./js/db.js');
+var bodyParser = require('body-parser');
 
 // Database 연동 
-db.connect(); 
+db.connect();
 
 app.use(express.static('public'));
 
-app.use(express.urlencoded({ extended:false }));
+app.use(express.urlencoded({ extended: false }));
 
 // 회원가입 폼에서 넘어온 post값을 받는 부분 
-app.post("/", function(req,res) {
-   var body = req.body; 
-   var fname = body.fname; 
-   var lname = body.name; 
-   var email = body.email2; 
-   var password = body.new_pw; 
-   var gender = body.chk_gender; 
-   var year = body.year;
-   var month = body.month;
-   var day = body.day;
+app.post("/", function (req, res) {
+    var body = req.body;
+    var fname = body.fname;
+    var lname = body.name;
+    var email = body.email2;
+    var password = body.new_pw;
+    var gender = body.chk_gender;
+    var year = body.year;
+    var month = body.month;
+    var day = body.day;
 
-   // 회원 가입정보 insert 
-   var query =
-   db.query('insert into users (fname, lname, email, password, gender, year, month, day) values ("' + fname + '","' + lname + '","' + email + '","' + password + '","' + gender + '","' + year + '","' + month + '","' + day + '")', 
-    function(err, rows) {
-        if(err) {
-            console.log(err); 
+    var query = db.query('SELECT * FROM users WHERE email=?', email, function (err, data) {
+        if (err) {
+            console.log(err);
         } else {
-            res.send(`<script type="text/javascript">alert("가입 완료");window.location="/";</script>`);
+            if (data.length == 0) {
+                // 회원 가입정보 insert 
+                var query2 =
+                    db.query('insert into users (fname, lname, email, password, gender, year, month, day) values ("' + fname + '","' + lname + '","' + email + '","' + password + '","' + gender + '","' + year + '","' + month + '","' + day + '")',
+                        function (err, rows) {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                res.send(`<script type="text/javascript">alert("가입 완료");window.location="/";</script>`);
+                            }
+                        });
+            } else {
+                res.send(`<script type="text/javascript">alert("이메일이 중복됩니다.");window.location="/";</script>`);
+            }
         }
-   });
+    })
+
+
+
 });
 
-app.listen(3000, function() { 
+app.listen(3000, function () {
     console.log('Success');
 });
