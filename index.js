014995bf7b0c31b7c0845a7dbbdb491addba0f5a
정우찬 <vdncks25@naver.com>
2019-03-30 22:12:32 +0900

    Password encryption
    비밀번호 암호화

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-console.log('hello');
-
-var express = require('express');
-var app = express();
-var db = require('./js/db.js');
-var bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const db = require('./js/db.js');
+const bodyParser = require('body-parser');
+const bkfd2Password = require('pbkdf2-password'); // 암호화를 하기 위한 모듈 
+const hasher = bkfd2Password();
 
 // Database 연동 
 db.connect();
 
 app.use(express.static('public'));
 
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // 회원가입 폼에서 넘어온 post값을 받는 부분 
 app.post("/", function (req, res) {
@@ -24,16 +24,18 @@ app.post("/", function (req, res) {
     var month = body.month;
     var day = body.day;
 
-    // 이메일 중복체크를 위한 쿼리문 
-    var query = db.query('SELECT * FROM users WHERE email=?', email, function (err, data) {
-        if (err) {
-            console.log(err);
-        } else {
-            // where절에 대한 data의 길이를 통해서 중복 여부 확인 
-            if (data.length == 0) {
-                // 회원 가입정보 insert 
-                var query2 =
-                    db.query('insert into users (fname, lname, email, password, gender, year, month, day) values ("' + fname + '","' + lname + '","' + email + '","' + password + '","' + gender + '","' + year + '","' + month + '","' + day + '")',
+    // pbkdf2의 hasher 함수를 이용하여 암호화를 진행한다. 
+    hasher({ password: password }, function (err, pass, salt, hash) {
+        // 이메일 중복체크를 위한 쿼리문 
+        db.query('SELECT * FROM users WHERE email=?', email, function (err, data) {
+            if (err) {
+                console.log(err);
+            } else {
+                // where절에 대한 data의 길이를 통해서 중복 여부 확인 
+                if (data.length == 0) {
+                    // 회원 가입정보 insert 
+                    // password에는 암호화가 된 hash값을 넣고, 동시에 키역할을 해주는 salt를 같이 insert 해준다. 
+                    db.query('insert into users (fname, lname, email, password, gender, year, month, day, salt) values ("' + fname + '","' + lname + '","' + email + '","' + hash + '","' + gender + '","' + year + '","' + month + '","'  + day + '","' + salt + '")',
                         function (err, rows) {
                             if (err) {
                                 console.log(err);
@@ -41,39 +43,37 @@ app.post("/", function (req, res) {
                                 res.send(`<script type="text/javascript">alert("가입 완료");window.location="/";</script>`);
                             }
                         });
-            } else {
-                res.send(`<script type="text/javascript">alert("이메일이 중복됩니다.");window.location="/";</script>`);
+                } else {
+                    res.send(`<script type="text/javascript">alert("이메일이 중복됩니다.");window.location="/";</script>`);
+                }
             }
-        }
+        });
     });
 });
 
 // 로그인 폼에서 넘어온 post값을 받는 부분 
-app.post("/login", function(req, res) {
-    var body = req.body;
-    var email = body.email;
-    var password = body.pw; 
+app.post("/login", function (req, res) {
+    const body = req.body;
+    const email = body.email;
+    const password = body.pw;
     // db에 post로 넘어온 이메일 존재 여부 
-    var sql = 'SELECT * FROM users WHERE email=?'; 
-    db.query(sql, [email], function(err, results) {
-        if(err) {
+    let sql = 'SELECT * FROM users WHERE email=?';
+    db.query(sql, [email], function (err, rows, results) {
+        if (err) {
             console.log(err);
-        } 
-
+        }
         // 이메일이 존재하지 않을 때 
-        if(!results[0]) {
-            res.send(`<script type="text/javascript">alert("이메일 및 패스워드를 확인해 주세요.");window.location="/";</script>`);     
+        if (!results[0]) {
+            res.send(`<script type="text/javascript">alert("이메일 및 패스워드를 확인해 주세요.");window.location="/";</script>`);
         } else {
-            db.query('SELECT password FROM users WHERE email=?',[email], function(err, rows, data) {
-                if(err) {
-                    console.log(err);
+            let user_salt = rows[0].salt;
+            // login form으로 부터 넘어온 비밀번호를 db에 저장되어 있던 salt값을 통해 다시 암호화를 시켜준다. 
+            return hasher({password:password, salt:user_salt}, function(err, pass, salt, hash) {
+                // 암호화 된 hash값과 db에 저장되어 있는 hash값(password)를 비교하여 로그인 여부 판단
+                if(hash === rows[0].password) {
+                    res.send('ok');
                 } else {
-                    // 비밀번호 매칭 
-                    if(password === rows[0].password) {
-                        res.send('ok');
-                    } else {
-                        res.send('fail');
-                    }
+                    res.send('fail');
                 }
             });
         }
