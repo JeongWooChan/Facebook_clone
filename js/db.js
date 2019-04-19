6aa2630e59513ca84bb0a4775af22869fd34d506
정우찬 <vdncks25@naver.com>
2019-04-19 13:38:47 +0900

    Reset Facebook-clone
    페이스북 클론을 다시 시작하기 위해 reset

--- js/db.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-const db = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : 'wjddncks!', 
-    database : 'facebook'
-});
-// db.connect();
-module.exports = db;
