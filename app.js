733271e7a018636c967c97c3d8520f22a8ae2e1c
정우찬 <vdncks25@naver.com>
2019-04-19 15:10:06 +0900

    SET : Build basic node.js environment for development
    express, babel, nodemon 등 기본적인 nodejs 환경 구축

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express"; 
+import helmet from "helmet";
+import bodyParser from "body-parser"; 
+import routes from "./routes"; 
+import globalRouter from "./routers/globalRouter"; 
+
+const app = express(); 
+
+app.use(helmet()); // express 보안을 위한 미들웨어 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(routes.home, globalRouter); 
+
+export default app; 
---
+++ init.js
@@ -0,0 +1,12 @@
+import dotenv from "dotenv"; // 환경변수를 로드하기 위한 모듈 
+import app from "./app";
+
+dotenv.config(); 
+
+const PORT = process.env.PORT; 
+
+const handleListening = () => {
+    console.log(`Listening on : http://localhost:${PORT}`);
+}
+
+app.listen(PORT, handleListening); 
