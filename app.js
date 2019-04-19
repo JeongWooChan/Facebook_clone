a7dadf5877e287e28e783889c7a62463d0fa3da2
정우찬 <vdncks25@naver.com>
2019-04-19 20:19:26 +0900

    SET : Apply javascript and scss to webpack

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 app.set("view engine", "pug"); 
 
 app.use(helmet()); // express 보안을 위한 미들웨어 
+app.use("/static", express.static("static")); 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
