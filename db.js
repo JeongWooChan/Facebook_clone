9e77244a95f1e2a2370e259c1b0f12449d4e6080
정우찬 <vdncks25@naver.com>
2019-04-20 20:24:27 +0900

    DB : Connect mysql and nodejs

---
+++ db.js
@@ -0,0 +1,25 @@
+import mysql from "mysql"; 
+import dotenv from "dotenv"; 
+
+dotenv.config(); 
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT, 
+    database: process.env.DB_DATABASE
+}); 
+
+const handleConnect = () => {
+    console.log("✅  Connected to DB");
+}
+
+connection.connect(err => {
+    if (err) {
+        console.log(err.stack); 
+    } else {
+        handleConnect();
+    }
+}); 
+
--- init.js
+++ init.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv"; // 환경변수를 로드하기 위한 모듈 
 import app from "./app";
+import "./db";
 
 dotenv.config(); 
 
