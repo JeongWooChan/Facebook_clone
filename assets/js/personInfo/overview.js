7a1f333f139fe246c0c174babecd8d6174ce19e2
정우찬 <vdncks25@naver.com>
2019-05-02 19:14:52 +0900

    JS : Add drop menu effect

--- assets/js/personInfo/overview.js
+++ assets/js/personInfo/overview.js
@@ -34,21 +34,110 @@ const editResidence = document.getElementById("menu1_residence_edit");
 // 출신지 수정 
 const editHometown = document.getElementById("menu1_hometown_edit");
 
+// dropdown 아이콘 
+const personInfo_infoWrap_menu = document.getElementById("personInfo_infoWrap_menu");
+const menu1_dropdown = document.getElementById("menu1_dropdown"); 
+const menu2_dropdown = document.getElementById("menu2_dropdown"); 
+const menu3_dropdown = document.getElementById("menu3_dropdown"); 
+const menu4_dropdown = document.getElementById("menu4_dropdown"); 
+const menu5_dropdown = document.getElementById("menu5_dropdown"); 
+
+let curruentPage; 
+
+const mqHandleMenu5 = () => {
+    menu1.style.display = "block";
+    menu2.style.display = "block";
+    menu3.style.display = "block";
+    menu4.style.display = "block";
+
+    menu1.style.paddingLeft = "40px";
+    menu2.style.paddingLeft = "40px";
+    menu3.style.paddingLeft = "40px";
+    menu4.style.paddingLeft = "40px";
+    menu5.style.paddingLeft = "90px";
+}
+
+const mqHandleMenu4 = () => {
+    menu1.style.display = "block";
+    menu2.style.display = "block";
+    menu3.style.display = "block";
+    menu5.style.display = "block";
+
+    menu1.style.paddingLeft = "40px";
+    menu2.style.paddingLeft = "40px";
+    menu3.style.paddingLeft = "40px";
+    menu4.style.paddingLeft = "90px";
+    menu5.style.paddingLeft = "40px";
+}
+
+const mqHandleMenu3 = () => {
+    menu1.style.display = "block";
+    menu2.style.display = "block";
+    menu4.style.display = "block";
+    menu5.style.display = "block";
+
+    menu1.style.paddingLeft = "40px";
+    menu2.style.paddingLeft = "40px";
+    menu3.style.paddingLeft = "90px";
+    menu4.style.paddingLeft = "40px";
+    menu5.style.paddingLeft = "40px";
+}
+
+const mqHandleMenu2 = () => {
+    menu1.style.display = "block";
+    menu3.style.display = "block";
+    menu4.style.display = "block";
+    menu5.style.display = "block";
+
+    menu1.style.paddingLeft = "40px";
+    menu2.style.paddingLeft = "90px";
+    menu3.style.paddingLeft = "40px";
+    menu4.style.paddingLeft = "40px";
+    menu5.style.paddingLeft = "40px";
+}
+
+const mqHandleMenu1 = () => {
+    menu2.style.display = "block";
+    menu3.style.display = "block";
+    menu4.style.display = "block";
+    menu5.style.display = "block";
+
+    menu1.style.paddingLeft = "90px";
+    menu2.style.paddingLeft = "40px";
+    menu3.style.paddingLeft = "40px";
+    menu4.style.paddingLeft = "40px";
+    menu5.style.paddingLeft = "40px";
+}
+
 const handleMenu1 = () => {
-    // display 
+    curruentPage = 1; 
+
     menu1Block.style.display = "block";
     menu2Block.style.display = "none";
     menu3Block.style.display = "none";
     menu4Block.style.display = "none";
     menu5Block.style.display = "none";
 
-    // border 
-    menu1.style.borderLeft = "4px solid black";
-    menu2.style.borderLeft = "none"; 
-    menu3.style.borderLeft = "none"; 
-    menu4.style.borderLeft = "none"; 
-    menu5.style.borderLeft = "none"; 
+    if(!matchMedia("(max-width: 768px").matches) {
+        // border 
+        menu1.style.borderLeft = "4px solid black";
+        menu2.style.borderLeft = "none"; 
+        menu3.style.borderLeft = "none"; 
+        menu4.style.borderLeft = "none"; 
+        menu5.style.borderLeft = "none"; 
+    } else {
+        menu1.style.display = "inline-block";
+        menu2.style.display = "none";
+        menu3.style.display = "none";
+        menu4.style.display = "none";
+        menu5.style.display = "none";
 
+        menu1_dropdown.style.display = "inline-block";
+        menu2_dropdown.style.display = "none";
+        menu3_dropdown.style.display = "none";
+        menu4_dropdown.style.display = "none";
+        menu5_dropdown.style.display = "none";
+    }
 
     //font 
     menu1.style.color = "black";
@@ -64,19 +153,34 @@ const handleMenu1 = () => {
 }
 
 const handleMenu2 = () => {
+    curruentPage = 2;
+
     menu1Block.style.display = "none";
     menu2Block.style.display = "block";
     menu3Block.style.display = "none";
     menu4Block.style.display = "none";
     menu5Block.style.display = "none";
 
-    // border 
-    menu1.style.borderLeft = "none";
-    menu2.style.borderLeft = "4px solid black"; 
-    menu3.style.borderLeft = "none"; 
-    menu4.style.borderLeft = "none"; 
-    menu5.style.borderLeft = "none"; 
+    if(!matchMedia("(max-width: 768px").matches) {
+        // border 
+        menu1.style.borderLeft = "none";
+        menu2.style.borderLeft = "4px solid black"; 
+        menu3.style.borderLeft = "none"; 
+        menu4.style.borderLeft = "none"; 
+        menu5.style.borderLeft = "none"; 
+    } else {
+        menu1.style.display = "none";
+        menu2.style.display = "inline-block";
+        menu3.style.display = "none";
+        menu4.style.display = "none";
+        menu5.style.display = "none";
 
+        menu1_dropdown.style.display = "none";
+        menu2_dropdown.style.display = "inline-block";
+        menu3_dropdown.style.display = "none";
+        menu4_dropdown.style.display = "none";
+        menu5_dropdown.style.display = "none";
+    }
 
     //font 
     menu1.style.color = "#90949C";
@@ -92,20 +196,35 @@ const handleMenu2 = () => {
 }
 
 const handleMenu3 = () => {
+    curruentPage = 3;
+
     menu1Block.style.display = "none";
     menu2Block.style.display = "none";
     menu3Block.style.display = "block";
     menu4Block.style.display = "none";
     menu5Block.style.display = "none";
 
-    // border 
-    menu1.style.borderLeft = "none";
-    menu2.style.borderLeft = "none"; 
-    menu3.style.borderLeft = "4px solid black"; 
-    menu4.style.borderLeft = "none"; 
-    menu5.style.borderLeft = "none"; 
+    if(!matchMedia("(max-width: 768px").matches) {
+        // border 
+        menu1.style.borderLeft = "none";
+        menu2.style.borderLeft = "none"; 
+        menu3.style.borderLeft = "4px solid black"; 
+        menu4.style.borderLeft = "none"; 
+        menu5.style.borderLeft = "none"; 
+    } else {
+        menu1.style.display = "none";
+        menu2.style.display = "none";
+        menu3.style.display = "inline-block";
+        menu4.style.display = "none";
+        menu5.style.display = "none";
 
-
+        menu1_dropdown.style.display = "none";
+        menu2_dropdown.style.display = "none";
+        menu3_dropdown.style.display = "inline-block";
+        menu4_dropdown.style.display = "none";
+        menu5_dropdown.style.display = "none";
+    }
+    
     //font 
     menu1.style.color = "#90949C";
     menu1.style.fontWeight = "normal";
@@ -120,19 +239,34 @@ const handleMenu3 = () => {
 }
 
 const handleMenu4 = () => {
+    curruentPage = 4;
+
     menu1Block.style.display = "none";
     menu2Block.style.display = "none";
     menu3Block.style.display = "none";
     menu4Block.style.display = "block";
     menu5Block.style.display = "none";
 
-    // border 
-    menu1.style.borderLeft = "none";
-    menu2.style.borderLeft = "none"; 
-    menu3.style.borderLeft = "none"; 
-    menu4.style.borderLeft = "4px solid black"; 
-    menu5.style.borderLeft = "none"; 
+    if(!matchMedia("(max-width: 768px").matches) {
+        // border 
+        menu1.style.borderLeft = "none";
+        menu2.style.borderLeft = "none"; 
+        menu3.style.borderLeft = "none"; 
+        menu4.style.borderLeft = "4px solid black"; 
+        menu5.style.borderLeft = "none"; 
+    } else {
+        menu1.style.display = "none";
+        menu2.style.display = "none";
+        menu3.style.display = "none";
+        menu4.style.display = "inline-block";
+        menu5.style.display = "none";
 
+        menu1_dropdown.style.display = "none";
+        menu2_dropdown.style.display = "none";
+        menu3_dropdown.style.display = "none";
+        menu4_dropdown.style.display = "inline-block";
+        menu5_dropdown.style.display = "none";
+    }
 
     //font 
     menu1.style.color = "#90949C";
@@ -148,19 +282,35 @@ const handleMenu4 = () => {
 }
 
 const handleMenu5 = () => {
+    curruentPage = 5;
+
     menu1Block.style.display = "none";
     menu2Block.style.display = "none";
     menu3Block.style.display = "none";
     menu4Block.style.display = "none";
     menu5Block.style.display = "block";
 
-    // border 
-    menu1.style.borderLeft = "none";
-    menu2.style.borderLeft = "none"; 
-    menu3.style.borderLeft = "none"; 
-    menu4.style.borderLeft = "none"; 
-    menu5.style.borderLeft = "4px solid black"; 
+    // mediaquery의 max-width 값이 768이 아닐때만 메뉴 옆에 border를 만들어줌 
+    if(!matchMedia("(max-width: 768px").matches) {
+        // border 
+        menu1.style.borderLeft = "none";
+        menu2.style.borderLeft = "none"; 
+        menu3.style.borderLeft = "none"; 
+        menu4.style.borderLeft = "none"; 
+        menu5.style.borderLeft = "4px solid black"; 
+    } else {
+        menu1.style.display = "none";
+        menu2.style.display = "none";
+        menu3.style.display = "none";
+        menu4.style.display = "none";
+        menu5.style.display = "inline-block";
 
+        menu1_dropdown.style.display = "none";
+        menu2_dropdown.style.display = "none";
+        menu3_dropdown.style.display = "none";
+        menu4_dropdown.style.display = "none";
+        menu5_dropdown.style.display = "inline-block";
+    }
 
     //font 
     menu1.style.color = "#90949C";
@@ -173,6 +323,45 @@ const handleMenu5 = () => {
     menu4.style.fontWeight = "normal";
     menu5.style.color = "black";
     menu5.style.fontWeight = "bold";
+}
+
+const onResize = () => {
+    if(matchMedia("(max-width: 768px)").matches) {
+        menu1.style.borderLeft = "none";
+        menu2.style.borderLeft = "none"; 
+        menu3.style.borderLeft = "none"; 
+        menu4.style.borderLeft = "none"; 
+        menu5.style.borderLeft = "none"; 
+    } else if (matchMedia("(min-width: 768px) and (max-width: 1024px)").matches) {
+        menu1.style.display = "block";
+        menu2.style.display = "block";
+        menu3.style.display = "block";
+        menu4.style.display = "block";
+        menu5.style.display = "block";
+
+        menu1_dropdown.style.display = "none";
+        menu2_dropdown.style.display = "none";
+        menu3_dropdown.style.display = "none";
+        menu4_dropdown.style.display = "none";
+        menu5_dropdown.style.display = "none"; 
+
+        menu1.style.paddingLeft = "40px";
+        menu2.style.paddingLeft = "40px";
+        menu3.style.paddingLeft = "40px";
+        menu4.style.paddingLeft = "40px";
+        menu5.style.paddingLeft = "40px";
+    } 
+    if(curruentPage == 1) {
+        handleMenu1();
+    } else if (curruentPage == 2) {
+        handleMenu2();
+    } else if (curruentPage == 3) {
+        handleMenu3();
+    } else if (curruentPage == 4) {
+        handleMenu4(); 
+    } else if (curruentPage == 5) {
+        handleMenu5(); 
+    }
 }
 
 const init = () => {
@@ -215,8 +404,16 @@ const init = () => {
     }
 
     // 생일 수정 클릭 이벤트 
-    editBirthday.addEventListener("click", handleMenu4);  
+    editBirthday.addEventListener("click", handleMenu4); 
 
+    // dropdown 
+    menu1_dropdown.addEventListener("click", mqHandleMenu1);
+    menu2_dropdown.addEventListener("click", mqHandleMenu2);
+    menu3_dropdown.addEventListener("click", mqHandleMenu3);
+    menu4_dropdown.addEventListener("click", mqHandleMenu4);
+    menu5_dropdown.addEventListener("click", mqHandleMenu5);
+
+    window.addEventListener("resize", onResize);
 }
 
 if(personInfo) {
