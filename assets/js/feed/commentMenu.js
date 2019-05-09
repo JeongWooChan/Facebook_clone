7a708dc81249e1c5c4cf1a34852343168d4d56d5
정우찬 <vdncks25@naver.com>
2019-05-09 22:52:36 +0900

    PUG/CSS/JS : Make comment edit form and add edit toggle function

--- assets/js/feed/commentMenu.js
+++ assets/js/feed/commentMenu.js
@@ -5,6 +5,38 @@ const commentMenuIcon = document.getElementsByClassName("commentMenu_icon");
 const commentMenu = document.getElementsByClassName("menu"); 
 const commentDeleteBtn = document.getElementsByClassName('comment_deleteText');
 const commentId = document.getElementsByClassName('comment_commentId'); 
+const commentEditBtn = document.getElementsByClassName('comment_editText'); 
+const editComment_cancel = document.getElementsByClassName("editComment_cancelDiv");
+
+// 기존 댓글 div 
+const commentDiv = document.getElementsByClassName("registered_comments_comment"); 
+const commentDiv2 = document.getElementsByClassName("registered_comments_menu");
+const commentContent = document.getElementsByClassName("registered_comment_content");
+// 수정 div 
+const editComment_Div = document.getElementsByClassName("editCommentDiv"); 
+const editCommentInput = document.getElementsByClassName("editComment_input");
+
+ const editCancel = i => {
+    commentDiv[i].style.display = "block";
+    commentDiv2[i].style.display = "block"; 
+    commentMenuIcon[i].style.display = "block";
+
+    editComment_Div[i].style.display = "none";
+    editComment_cancel[i].style.display = "none";
+ }
+
+const editComment = i => {
+
+    commentDiv[i].style.display = "none";
+    commentDiv2[i].style.display = "none"; 
+    commentMenuIcon[i].style.display = "none";
+
+    editComment_Div[i].style.display = "block";
+    editComment_cancel[i].style.display = "block";
+    editCommentInput[i].value = commentContent[i].innerHTML;
+
+    editCommentInput[i].focus();
+}
 
 const deleteComment = async i => {
     const id = commentId[i].innerHTML;
@@ -32,6 +64,10 @@ const init = () => {
         commentMenuIcon[i].addEventListener("click", toggleMenu.bind(null, i), false); 
         // 댓글 삭제 
         commentDeleteBtn[i].addEventListener("click", deleteComment.bind(null, i), false);
+        // 댓글 수정 
+        commentEditBtn[i].addEventListener("click", editComment.bind(null, i), false);
+        // 수정 취소 
+        editComment_cancel[i].addEventListener("click", editCancel.bind(null,i), false);
     }
 
     document.addEventListener("click", function(e) {
