5685689783c7c7e442c3e0bde10fe0992739cd21
정우찬 <vdncks25@naver.com>
2019-05-12 23:55:38 +0900

    JS : Change from reload to fake addReply

--- assets/js/feed/reply.js
+++ assets/js/feed/reply.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import "./replyMenu";
 
 const comment = document.getElementsByClassName("registered_comments"); 
 const writeReplyBtn = document.getElementsByClassName("write_reply_button"); 
@@ -6,7 +7,64 @@ const replyDiv = document.getElementsByClassName("editComment_replyDiv");
 const replyDiv_cancel = document.getElementsByClassName("editComment_Reply_cancel");
 const replyForm = document.getElementsByClassName("replyForm");
 
-const sendReply = async (comment, id, feedId) => {
+const fakeAddReply = (comment, i) => {
+    const avatar = document.getElementsByClassName("addCommentForm_avatar");
+    const name = document.getElementById("main_header_profileName").innerHTML;
+
+    // CreateElement
+    const registered_comments = document.getElementsByClassName("registered_comments");
+    const registered_reply = document.createElement("div"); 
+
+    const replyPart = document.createElement("div"); 
+
+    const registered_reply_img = document.createElement("div"); 
+    const registered_reply_reply = document.createElement("div"); 
+    const icon = document.createElement("i"); 
+    const editReplyDiv = document.createElement("div"); 
+    const editReply_cancel = document.createElement("div"); 
+
+    const image = document.createElement("img");
+    const userName = document.createElement("a");
+    const replyContent = document.createElement("span"); 
+
+    // className 
+    registered_reply.className="registered_reply";
+
+    replyPart.className = "replyPart"; 
+
+    registered_reply_img.className = "registered_reply_img";
+    registered_reply_reply.className = "registered_reply_reply";
+    icon.className = "fas fa-ellipsis-h reply_menuIcon"; 
+    editReplyDiv.className = "editReplyDiv";
+    editReply_cancel.className = "editReply_cancel";
+
+    userName.className = "registered_reply_name";
+    replyContent.className = "registered_reply_content";
+
+    // input value 
+    image.src=avatar[i].src;
+    userName.href="#";
+    userName.innerHTML = name;
+    replyContent.innerHTML = comment;
+
+    //append 
+    registered_reply_img.appendChild(image);
+
+    registered_reply_reply.appendChild(userName);
+    registered_reply_reply.appendChild(replyContent);
+
+    replyPart.appendChild(registered_reply_img);
+    replyPart.appendChild(registered_reply_reply);
+    replyPart.appendChild(icon);
+    replyPart.appendChild(editReplyDiv);
+    replyPart.appendChild(editReply_cancel);
+    
+    registered_reply.appendChild(replyPart);
+
+    registered_comments[i].appendChild(registered_reply);
+}
+
+const sendReply = async (comment, id, feedId, i) => {
     const response = await axios({
         url : `/api/${id}/reply`,
         method: "POST",
@@ -16,7 +74,9 @@ const sendReply = async (comment, id, feedId) => {
         }
     });
     if(response.status === 200) {
-        window.location.reload(true);
+        replyDiv[i].style.display = "none";
+        
+        fakeAddReply(comment, i);
     }
 }
 
@@ -28,7 +88,7 @@ const handleSubmit = async (i, event) => {
     const comment = commentInput[i].value;
     const feedId = document.getElementsByClassName("feed_Id")[i].innerHTML;
 
-    sendReply(comment, commentId, feedId); 
+    sendReply(comment, commentId, feedId, i); 
 }
 
 const cancelWriteReply = i => {
