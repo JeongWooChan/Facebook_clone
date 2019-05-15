a45df907a3d1e1fe4d72bbce73c6a3ee778bf6af
정우찬 <vdncks25@naver.com>
2019-05-15 21:11:51 +0900

    JS/CSS : Make friend box frame
    웹 브라우저 resize에 반응하게끔 만듬

--- assets/js/header.js
+++ assets/js/header.js
@@ -5,6 +5,25 @@ const logoText = document.getElementById("header_section3_logoName");
 const menuIcon = document.getElementById("section3_menu_icon"); 
 const headerMenu = document.getElementById("header_menu");
 
+const headerFriendBtn = document.getElementById("header_friend_button");
+const headerFriendBox = document.getElementById("header_friend_box"); 
+
+
+
+const toggleFriendBox = e => {
+
+    if(headerFriendBox.style.visibility == "hidden") {
+        headerFriendBox.style.visibility = "visible";
+        headerFriendBox.style.left = headerFriendBtn.offsetLeft - 180 + "px";
+    } else {
+        headerFriendBox.style.visibility = "hidden";
+    }
+}
+
+const responsiveFriendBox = () => {
+    headerFriendBox.style.left = headerFriendBtn.offsetLeft - 180 + "px";
+}
+
 const toggleMenu = () => {
     if(headerMenu.style.display == "none") {
         headerMenu.style.display = "block";
@@ -22,6 +41,8 @@ const showSearchForm = () => {
 const init = () => {
     searchIcon.addEventListener("click", showSearchForm);
     menuIcon.addEventListener("click", toggleMenu); 
+    headerFriendBtn.addEventListener("click", toggleFriendBox);
+    window.addEventListener("resize", responsiveFriendBox);
 }
 
 if(header) {
