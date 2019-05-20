e0f8874d8ba843aaa50ad978e720ed262c6af47a
정우찬 <vdncks25@naver.com>
2019-05-20 14:25:36 +0900

    NODE.JS/JS : Fail autoPaging
    오토 페이징의 기능 자체는 구현했었지만, 오토 페이징을 통해 생겨난 피드에 click event및 오류들이 많음

--- assets/js/mainPage.js
+++ assets/js/mainPage.js
@@ -1,85 +1,8 @@
-import axios from "axios";
-import routes from "../../routes";
-
 const main = document.getElementById("main_contents"); 
 const feedSection = document.getElementById("main_contentFeedSection");
 const addSection = document.getElementById("main_content_adSection"); 
 
 const searchForm = document.getElementById("header_searchForm");
-
-
-const jsAddFeedBlock = feed => {
-    console.log(feed);
-
-    const feedBlock = `
-    <section class="main_content_feedSection">
-        <article class="main_content_feed">
-            <div class="main_content_header">
-                <img src='/${feed.avatarUrl}'/>
-                <div class="main_content_headerTop">
-                    <a href='${routes.person(feed.userId)}' id="main_content_writer">
-                        ${feed.username}
-                    </a>
-                    <div class="main_content_more">
-                        <i class="fas fa-ellipsis-h"></i>
-                    </div> 
-                </div>
-                <div class="main_content_headerBottom">
-                    <span class="main_content_time" hidden="hidden">
-                        ${feed.date}
-                    </span>
-                    <span class="main_time_gap">
-                    </span>
-                </div>
-                <div class="main_menuDiv" style="display: none;">
-                    <ul>
-                       
-                    </ul>
-                </div>
-            </div>
-        </article>
-    </section>
-    `;
-    document.getElementById("main_contentFeedSection").insertAdjacentHTML('beforeend', feedBlock);
-}
-
-const autoPaging = async () => {
-    // const feedBlock = `
-    // <section class= main_content_feedSection>
-    //     <article class="main_content_feed">
-    //         <div class="main_content_header">
-    //         </div>
-    //     </article>
-    // </section>`;
-
-    // document.getElementById("main_contentFeedSection").insertAdjacentHTML('beforeend', feedBlock);
-    // console.log(document.getElementById("autopaging_number").value);
-    // document.getElementById("autopaging_number").value++;
-
-    const pagingNumber = document.getElementById("autopaging_number").value; 
-
-    const response = await axios({
-        url:`/api/${routes.feedAuto}`, 
-        method: "POST", 
-        data: {
-            pagingNumber
-        }
-    }); 
-
-    if(response.status === 200) {
-        const feedList = response.data.feed;
-
-        for(let i = 0; i < feedList.length; i++) {
-            jsAddFeedBlock(feedList[i]); 
-        }
-    }
-}
-
-const scrollEnd = () => {
-    if((window.scrollY + window.innerHeight) / document.body.clientHeight >= 1) {
-        autoPaging();
-    }
-}
 
 const inputvalidate = e => {
     e.preventDefault();
@@ -111,8 +34,6 @@ const init = () => {
     window.addEventListener("resize", responsiveAddBox); 
     window.addEventListener("load", load);
     searchForm.addEventListener("submit", inputvalidate);
-
-    window.addEventListener("scroll", scrollEnd); 
 }
 
 if(main) {
